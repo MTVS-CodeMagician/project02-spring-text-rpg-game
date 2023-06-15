03093bd885c9452c18da05029dcd24add0cdab6c
jinvvoo <jinvvookim@gmail.com>
2023-06-15 17:39:23 +0900

    feature/DTO 작성 (#8)

--- src/main/java/com/ohjiraffers/project02springtextrpggame/command/dto/MonsterDTO.java
+++ src/main/java/com/ohjiraffers/project02springtextrpggame/command/dto/MonsterDTO.java
@@ -1,4 +1,17 @@
 package com.ohjiraffers.project02springtextrpggame.command.dto;
 
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
 public class MonsterDTO {
+
+    private String monName;
+    private int monStone;
+    private int monAttPer;
 }
--- src/main/java/com/ohjiraffers/project02springtextrpggame/command/infra/database/UserDB.java
+++ src/main/java/com/ohjiraffers/project02springtextrpggame/command/infra/database/UserDB.java
@@ -1,4 +1,12 @@
 package com.ohjiraffers.project02springtextrpggame.command.infra.database;
 
+import com.ohjiraffers.project02springtextrpggame.command.dto.UserDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class UserDB {
+
+    private final List<UserDTO> userDTOList = new ArrayList<>();
 }
