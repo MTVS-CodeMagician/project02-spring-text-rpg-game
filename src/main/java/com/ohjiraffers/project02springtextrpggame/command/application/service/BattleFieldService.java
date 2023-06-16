4346d49575c6af7824f9359cce337351fdaabf36
yongmin jin <jym7546@gmail.com>
2023-06-16 16:30:16 +0900

    Battlefiled 작성 및 monster interface수정(#10)
    몬스터 서비스 class 수정 및 battlefield logic 작성

--- src/main/java/com/ohjiraffers/project02springtextrpggame/command/application/service/BattleFieldService.java
+++ src/main/java/com/ohjiraffers/project02springtextrpggame/command/application/service/BattleFieldService.java
@@ -1,4 +1,41 @@
 package com.ohjiraffers.project02springtextrpggame.command.application.service;
 
+import com.ohjiraffers.project02springtextrpggame.command.domain.entity.UserEntity;
+import com.ohjiraffers.project02springtextrpggame.command.domain.repository.UserRepository;
+import com.ohjiraffers.project02springtextrpggame.command.domain.service.MonsterService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class BattleFieldService {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.ohjiraffers.project02springtextrpggame.command");
+        AllMonsterService allMonsterService = context.getBean("monsterService", AllMonsterService.class);
+//        System.out.println(allMonsterService.attack_monster(1));
+    public void battle(UserRepository setUser, UserEntity getUser) {
+//        MonsterDTO finalMonster=context.getBean("addMonster",MonsterDTO.class);
+//        System.out.println("finalMonster = " + finalMonster);
+        int rand = (int) (Math.random() * 3 + 1);
+        int countM = 0;
+        int countU = 0;
+        while (true) {
+            if (true/*User attack*/) {
+                countU++;
+                if (countU == 3) {
+                    break;
+                }
+            }
+            if (allMonsterService.attack_monster(rand)) {
+                countM++;
+                if (countM == 3) {
+                    break;
+                }
+            }
+        }
+      int stone=getUser.getUserStone();
+      int Mstone=allMonsterService.getStone(rand);
+
+    /*레벨업 함수*/
+    // 마을로 가는 method
+    }
 }
--- src/main/java/com/ohjiraffers/project02springtextrpggame/command/domain/service/MonsterAService.java
+++ src/main/java/com/ohjiraffers/project02springtextrpggame/command/domain/service/MonsterAService.java
@@ -1,7 +1,11 @@
 package com.ohjiraffers.project02springtextrpggame.command.domain.service;
 
 import com.ohjiraffers.project02springtextrpggame.command.dto.MonsterDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Repository("monsterAService")
 public class MonsterAService implements MonsterService {
     MonsterDTO monsterA=new MonsterDTO();
     public MonsterAService(){
@@ -12,8 +16,14 @@ public class MonsterAService implements MonsterService {
     // 나중에 몬스터 DB를 만들 때는 윗부분을 DB에 따로 분리해서 작성.
     @Override
     public boolean attack() {
+        System.out.println("몬스터 A 공격!!");
         double tempValue=Math.random();
         int perValue=(int)(tempValue*100);
         return perValue<monsterA.getMonAttPer()? true:false;
+    }
+
+    @Override
+    public int getStone() {
+        return monsterA.getMonStone();
     }
 }
