771a0c1d2d8bc7cd9c75ad6f98f598ad2ef45d98
taedy <taedyyyyy@gmail.com>
2023-06-17 13:15:34 +0900

    feature/게임 종료 로직 구현, 유저 빈 설정 코드 수정2(최종)

--- src/main/java/com/ohjiraffers/project02springtextrpggame/command/application/service/BattleFieldService.java
+++ src/main/java/com/ohjiraffers/project02springtextrpggame/command/application/service/BattleFieldService.java
@@ -53,7 +53,7 @@ public class BattleFieldService {
             System.out.println("현재 유저의 스톤개수는 : "+userEntity.getUserStone());
             userService.levelUp();
         }
-        if(userEntity.getUserLV() >= 2){
+        if(userEntity.getUserLV() >= 10){
             System.out.println("당신의 레벨이 10이 되었습니다.");
             System.out.println("ENTER 버튼을 누르면 게임이 종료됩니다. 이용해주셔서 감사합니다.");
             Scanner sc = new Scanner(System.in);
