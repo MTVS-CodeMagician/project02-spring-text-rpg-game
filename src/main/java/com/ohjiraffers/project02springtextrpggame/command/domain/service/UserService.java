c95e32294a344d455970e7fdeaf7353a92124b0f
yongmin jin <jym7546@gmail.com>
2023-06-19 09:02:10 +0900

    final

--- src/main/java/com/ohjiraffers/project02springtextrpggame/command/domain/service/UserService.java
+++ src/main/java/com/ohjiraffers/project02springtextrpggame/command/domain/service/UserService.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class UserService {
-
-    private UserRepository userRepository;
-    private UserEntity userEntity;
-
+    UserEntity userEntity=new UserEntity();
+    UserRepository userRepository=new UserRepository();
 
     //유저 레벨과 스톤의 초깃값
     public UserService(){
@@ -55,59 +53,3 @@ public class UserService {
     }
 
 }
-
-
-
-
-
-
-
-
-
-/* 이하 필기용. 지워도 됨 */
-
-//
-//    public void setUserLv(int lv) {
-//        this.userLv = lv;
-//    }
-//
-//    public int getUserLv() {
-//        return this.userLv;
-//    }
-
-
-
-
-//    public boolean levelUp(int userStone, int userLv) {
-//        if (userStone < 3) {
-//            return false;
-//        } else if (userStone >= 3) {
-//            userLv =+ 1;
-//            System.out.println("레벨업에 성공하였습니다!");
-//            return true;
-//        }
-//    }
-
-
-
-
-
-
-// stone 갯수 출력
-//name
-//stone
-
-
-
-//    public int levelUp(int userStone, int userLv) {
-//        if(userStone < 3) {
-//            //
-//        } else if (userStone >= 3) {
-//            userLv =+ 1;
-//            System.out.println("레벨업에 성공하였습니다!");
-//            return userLv;
-//            return userStone = 0;
-//        }
-//    }
-
-
